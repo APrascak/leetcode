c95a5e1f626774274a3423cbe3c8891eef2ab0c6
Alex Prascak <aprascak@ufl.edu>
2020-04-10 22:56:07 -0400

    Binary Tree Postorder Traversal

---
+++ 145.java
@@ -0,0 +1,44 @@
+// Binary Tree Postorder Traversal
+// 10 April 2020
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> order = new ArrayList<>();
+        Stack<TreeNode> s = new Stack<>();
+        Set<TreeNode> set = new HashSet<>();
+        if (root == null) return order;
+        
+        s.push(root);
+        
+        
+        while (!s.isEmpty()) {
+            TreeNode curr = s.pop();
+            while (curr.left != null && !set.contains(curr.left)) {
+                s.push(curr);
+                set.add(curr);
+                curr = curr.left;
+            }
+            if (curr.right != null && !set.contains(curr.right)) {
+                s.push(curr);
+                set.add(curr);
+                s.push(curr.right);
+                continue;
+            }
+            set.add(curr);
+            order.add(curr.val);
+        }
+        
+        return order;
+        
+    }
+    
+}
