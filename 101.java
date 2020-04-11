a6e4733c80845fb0a39fae789324aa10f16c0a9c
Alex Prascak <aprascak@ufl.edu>
2020-04-10 22:45:44 -0400

    Symmetric Tree

---
+++ 101.java
@@ -0,0 +1,23 @@
+// Symmetric Tree
+// 10 April 2020
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+       return mirror(root, root); 
+    }
+    
+    public boolean mirror(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+        return (a.val == b.val) && mirror(a.right, b.left) && mirror(a.left, b.right);
+    }
+}
