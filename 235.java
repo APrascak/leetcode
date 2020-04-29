184b9367efb23f90e001f777a77e59fb4ce0d5b3
Alex Prascak <aprascak@ufl.edu>
2020-04-29 15:10:39 -0400

    Lowest Common Ancestor of a Binary Search Tree

---
+++ 235.java
@@ -0,0 +1,42 @@
+// Lowest Common Ancestor of a Binary Search Tree
+// 29 April 2020
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// Iterative Solution: O(n) time and O(1) space
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.val < p.val && curr.val < q.val) {
+                curr = curr.right;
+            } else if (curr.val > q.val && curr.val > p.val) {
+                curr = curr.left;
+            } else {
+                return curr;
+            }
+        }
+        return curr;
+    }
+}
+
+// Recursive solution: O(n) time and O(n) space (for the recursive stack)
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
