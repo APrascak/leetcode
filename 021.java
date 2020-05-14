5cd7afff9c54ce2b7cce69e7cb5cffc142cecc11
Alex Prascak <aprascak@ufl.edu>
2020-05-13 21:39:22 -0400

    Merge Two Sorted Lists

--- 021.java
+++ 021.java
@@ -1,6 +1,26 @@
 // Merge Two Sorted Lists
 // 9 March 2020
 
+// Recusrive: O(n) time and space (Updated 13 May 2020)
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null)
+            return null;
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+// Iterative: O(n) time and space
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         
