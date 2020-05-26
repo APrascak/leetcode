f9ec132f3fb29b8b4f6e3085f6c17ec9913f7809
Alex Prascak <aprascak@ufl.edu>
2020-05-25 20:07:57 -0400

    Missing Number

--- 268.java
+++ 268.java
@@ -1,6 +1,19 @@
 // Missing Number
 // 1 April 2020
 
+// O(n) time O(1) space: 25 May 2020
+class Solution {
+    public int missingNumber(int[] nums) {
+        int total = 0, curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += i+1;
+            curr += nums[i];
+        }
+        return total - curr;
+    }
+}
+
+// O(n) time O(n) space
 class Solution {
     public int missingNumber(int[] nums) {
         Set<Integer> seen = new HashSet<Integer>();
